0c68b89f76cf962b7e20523f697d28cfdc4d2aa2
guivictorr <55333929+guivictorr@users.noreply.github.com>
2022-05-13 11:32:47 -0300

    feat(fonts): adding roboto font for the project

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,14 @@
-import styles from '../styles/Home.module.scss';
+import Head from 'next/head';
+import styles from 'styles/home.module.scss';
 const Home = () => {
-  return <h1 className={styles.home}>hello</h1>;
+  return (
+    <>
+      <Head>
+        <title>ignews</title>
+      </Head>
+      <h1 className={styles.home}>hello</h1>
+    </>
+  );
 };
 
 export default Home;
