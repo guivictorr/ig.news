d85b4b8866b6fb737a4f11aa7c06e5bc5e2b0f1b
guivictorr <55333929+guivictorr@users.noreply.github.com>
2022-05-15 01:36:08 -0300

    feat(subscription): getting info about subscription value from stripe

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,34 @@
+import { GetServerSideProps } from 'next';
+import { stripe } from 'services/stripe';
 import HomeTemplate from 'templates/Home';
 
-function Home() {
-  return <HomeTemplate />;
+export type HomeProps = {
+  priceData: {
+    id: string;
+    price: string;
+  };
+};
+
+function Home(props: HomeProps) {
+  return <HomeTemplate {...props} />;
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve('price_1KzYeREcbM5W9LXWqUGq86tP');
+
+  const priceData = {
+    id: price.id,
+    price: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format((price.unit_amount ?? 0) / 100),
+  };
+
+  return {
+    props: {
+      priceData,
+    },
+  };
+};
 
 export default Home;
--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -1,10 +1,11 @@
 import { SubscribeButton } from 'components/SubscribeButton';
 import Head from 'next/head';
 import Image from 'next/image';
+import { HomeProps } from '../../../pages';
 
 import styles from './styles.module.scss';
 
-function HomeTemplate() {
+function HomeTemplate({ priceData }: HomeProps) {
   return (
     <>
       <Head>
@@ -13,7 +14,7 @@ function HomeTemplate() {
 
       <main className={styles.contentContainer}>
         <section className={styles.hero}>
-          <p>
+          <p className={styles.greetings}>
             <Image
               src="/images/clap.png"
               width={26}
@@ -25,9 +26,9 @@ function HomeTemplate() {
           <h1>
             News about <br /> the <span>React</span> world
           </h1>
-          <p>
+          <p className={styles.pricing}>
             Get acess to all the publications <br />
-            <span>for $9.90 month</span>
+            <span>for {priceData.price} month</span>
           </p>
           <SubscribeButton />
         </section>
