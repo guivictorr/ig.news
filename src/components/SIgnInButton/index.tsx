c30e3812e75e7070a88221324e6f6ba187f671bc
guivictorr <55333929+guivictorr@users.noreply.github.com>
2022-05-16 22:57:17 -0300

    feat(auth): implementing github auth with next-auth

--- src/components/SIgnInButton/index.tsx
+++ src/components/SIgnInButton/index.tsx
@@ -1,15 +1,19 @@
+import { signIn, signOut, useSession } from 'next-auth/react';
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
 import styles from './styles.module.scss';
 
 export function SignInButton() {
-  const isUserLoggedIn = false;
-
+  const { status, data } = useSession();
+  const isAuthenticated = status === 'authenticated';
   return (
-    <button className={styles.signInButton}>
-      <FaGithub size={24} color={isUserLoggedIn ? '#04D361' : '#eba417'} />
-      <p>{isUserLoggedIn ? 'guivictorr' : 'Sign in with Github'}</p>
-      {isUserLoggedIn && <FiX size={24} color="#737380" />}
+    <button
+      className={styles.signInButton}
+      onClick={isAuthenticated ? () => signOut() : () => signIn('github')}
+    >
+      <FaGithub size={24} color={isAuthenticated ? '#04D361' : '#eba417'} />
+      <p>{isAuthenticated ? data?.user?.name : 'Sign in with Github'}</p>
+      {isAuthenticated && <FiX size={24} color="#737380" />}
     </button>
   );
 }
