1b560ec575adc7d8e46375d5f5256229b1be813c
guivictorr <55333929+guivictorr@users.noreply.github.com>
2022-05-17 19:40:13 -0300

    feat(authentication): verifying if user exist on database to save email

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,5 +1,5 @@
 import NextAuth from 'next-auth';
-import { query } from 'faunadb';
+import { query as q } from 'faunadb';
 import GithubProvider from 'next-auth/providers/github';
 import { fauna } from 'services/fauna';
 
@@ -13,11 +13,23 @@ export default NextAuth({
   secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
     async signIn({ user }) {
+      if (!user.email) {
+        return false;
+      }
+
       try {
         await fauna.query(
-          query.Create(query.Collection('users'), {
-            data: { email: user.email },
-          }),
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(q.Index('user_by_email'), q.Casefold(user.email)),
+              ),
+            ),
+            q.Create(q.Collection('users'), {
+              data: { email: user.email },
+            }),
+            q.Get(q.Match(q.Index('user_by_email'), q.Casefold(user.email))),
+          ),
         );
         return true;
       } catch (error) {
